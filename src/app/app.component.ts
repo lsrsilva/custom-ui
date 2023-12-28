f734ed527dd7ad9e289df504574c678175c591ee
Lucas da Silva Rezende <trab.lucasilva@gmail.com>
2023-12-28 00:59:19 -0300

    Change prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 @Component({
   standalone: true,
   imports: [NxWelcomeComponent, RouterModule],
-  selector: 'custom-ui-root',
+  selector: 'cui-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
